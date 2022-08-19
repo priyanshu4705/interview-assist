12e8fbbf3fba862af49b3bb78f038dd7c74044f7
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2022-08-19 20:22:27 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+        <h1>
+          <code>Hello world from interview assist 👋</code>
+        </h1>
+    </div>
+  );
+}
+
+export default App;
