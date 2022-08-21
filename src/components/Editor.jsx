4e3a837d326cdb6d11cfef782465c9f81dc49ce6
Pankaj <chaudharypankajatt0001@gmail.com>
2022-08-22 00:03:38 +0530

    dropdown added

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,19 @@
 import MonacoEditor from 'react-monaco-editor';
+import * as React from 'react';
+
 
 const Editor = () => {
+
+  const lanoptions = [
+    { label: 'C++', value: 'c++' },
+    { label: 'JavaScript', value: 'javascript' },
+    { label: 'Python', value: 'python' },
+  ];
+  const [currentlan, setCurrentlan] = React.useState('c++');
+  const handlelanChange = (e) => {
+    setCurrentlan(e.target.value);
+  };
+
   let value = [
     'function x() {',
     '\tconsole.log("Hello world!");',
@@ -11,16 +24,37 @@ const Editor = () => {
     console.log(value);
   }
   return (
+    <>
+      <Dropdown
+        label="Change Language"
+        options={lanoptions}
+        value={currentlan}
+        onChange={handlelanChange}
+      />
+      <p>  current lan : {currentlan}</p>
     <MonacoEditor
     width="800"
     height="600"
-    language="javascript"
+    language={currentlan}
     theme="vs-dark"
     value={value}
     onChange={handleEditorChange}
     
-  />
+      />
+      </>
   )
+};
+const Dropdown = ({ label, value, options, onChange }) => {
+  return (
+    <label>
+      {label}
+      <select value={value} onChange={onChange}>
+        {options.map((option, i) => (
+          <option key = {i} value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </label>
+  );
 };
 
 export default Editor;
