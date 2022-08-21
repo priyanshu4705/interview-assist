7957d2f63ae6beb481e7f760a1f509a0c04a00f3
Pankaj <chaudharypankajatt0001@gmail.com>
2022-08-21 23:34:25 +0530

    monaco works

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,26 @@
+import MonacoEditor from 'react-monaco-editor';
+
 const Editor = () => {
-  return <div>Editor</div>;
+  let value = [
+    'function x() {',
+    '\tconsole.log("Hello world!");',
+    '}'
+  ].join('\n');
+  let language = 'javascript';
+  const handleEditorChange = (value) => {
+    console.log(value);
+  }
+  return (
+    <MonacoEditor
+    width="800"
+    height="600"
+    language="javascript"
+    theme="vs-dark"
+    value={value}
+    onChange={handleEditorChange}
+    
+  />
+  )
 };
 
 export default Editor;
